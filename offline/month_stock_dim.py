66e99fdc5c68785f299093f3e94d2a4c621f6ac6
xiaobinwu <2363521277@qq.com>
2022-07-18 20:25:52 +0800

    【feat】获取个股详情数据

---
+++ offline/month_stock_dim.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import time
+
+import tushare as ts
+import pandas as pd
+from pandas import DataFrame
+from retrying import retry
+
+from common.logger import logger
+from common.tools import Sink
+
+pd.set_option('display.width', 200)
+pd.set_option('display.max_columns', None)
+
+ts.set_token("c20ea165fe87e91c0eec2f1fb529b29e0a1ce3ee61f017dc96f64c7b")
+pro = ts.pro_api()
+
+table_name = 't_stock_detail'
+
+
+# 个股详情(最多重试5次，每次间隔3秒)
+@retry(stop_max_attempt_number=5, wait_fixed=3000)
+def get_stock_detail() -> DataFrame:
+    stock_df: DataFrame = pro.stock_basic(
+        feilds='ts_code,symbol,name,area,industry,market,exchange,curr_type,'
+               'list_status,list_date,delist_date,is_hs')
+    rows = stock_df.shape[0]
+    logger.info("从 pro.stock_basic接口获取个股详情数据 %d行" % rows)
+    dt = time.strftime('%Y%m%d')
+    dt_col = [dt for _ in range(rows)]
+    # 原始数据中没有时间列
+    stock_df.insert(loc=0, column='dt', value=dt_col)
+
+    return stock_df
+
+
+def run():
+    stock = get_stock_detail()
+    Sink.df_to_mysql(stock, table_name)
+
+
+if __name__ == "__main__":
+    run()
